528f6030efda802901b3f0bbdb8fd3067eca0a05
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-09-21 16:45:49 +0200

    feat: added initial app
    A  .envrc
    A  .github/CODEOWNERS.md
    A  .github/CODE_OF_CONDUCT.md
    A  .github/CONTRIBUTING.md
    A  .github/ISSUE_TEMPLATE/bug_report.yml
    A  .github/ISSUE_TEMPLATE/feature_request.yml
    A  .github/SECURITY.md
    A  .github/dependabot.yml
    A  .github/policies/resourceManagement.yml
    A  .github/pull_request_template.md
    A  .github/workflows/dependabot_auto_merge.yaml
    A  .github/workflows/flake-test.yml
    A  .github/workflows/jekyll-gh-pages.yaml
    A  .github/workflows/security-scan.yaml
    A  .gitignore
    A  Makefile
    A  flake.lock
    A  flake.nix
    A  go.mod
    A  go.sum
    A  main.go
    A  sonar-project.properties

---
+++ .github/CODEOWNERS.md
@@ -0,0 +1,1 @@
+*            @IliyanKostov9
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Go server for ZProfile";
+
+  inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    devenv-root,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = {
+          name = "Go server for ZProfile";
+          languages.go.enable = true;
+
+          # NOTE: First do devenv shell
+          git-hooks.hooks = {
+            actionlint = {
+              enable = true;
+              excludes = ["docker-publish.yaml"];
+            };
+          };
+
+          devenv.root = let
+            devenvRootFileContent = builtins.readFile devenv-root.outPath;
+          in
+            pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
+
+          packages = with pkgs; [
+            # hello
+          ];
+          enterShell = ''
+            export PATH="$HOME/.local/share/nvim/mason/bin:$PATH"
+          '';
+        };
+      };
+    };
+}
