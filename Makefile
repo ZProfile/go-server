44a6550d05db7f070458224c2500a78f30d42e62
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-09-21 22:58:59 +0200

    feat: added keycloak env
    M  Makefile
    M  flake.nix
    A  keycloak/realms/zprofile.json

--- Makefile
+++ Makefile
@@ -1,3 +1,24 @@
-.PHONY: run
-run:
+###############
+# TARGETS
+###############
+
+.PHONY: help
+help:  ## help target to show available commands with information
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) |  awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+.PHONY: all services run
+all: services run
+
+run: ## Run the rest api app
 	go run main.go
+
+services: ## Run keycloak locally
+	devenv up -d
+
+stop: ## Stop keycloak
+	process-compose down
+
+.PHONY: keycloak-attach
+keycloak-attach:
+	process-compose attach
+
--- flake.nix
+++ flake.nix
@@ -40,6 +40,23 @@
           name = "Go server for ZProfile";
           languages.go.enable = true;
 
+          services.keycloak = {
+            enable = true;
+            initialAdminPassword = "admin";
+            sslCertificate = null;
+            sslCertificateKey = null;
+            realms.zprofile = {
+              # path = "./keycloak/realms/zprofile.json";
+              # import = true;
+              # export = true;
+            };
+            settings = {
+              hostname = "localhost";
+              http-port = 8080;
+              http-enabled = true;
+            };
+          };
+
           # NOTE: First do devenv shell
           git-hooks.hooks = {
             actionlint = {
@@ -52,13 +69,6 @@
             devenvRootFileContent = builtins.readFile devenv-root.outPath;
           in
             pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
-
-          packages = with pkgs; [
-            # hello
-          ];
-          enterShell = ''
-            export PATH="$HOME/.local/share/nvim/mason/bin:$PATH"
-          '';
         };
       };
     };
